9eece0200368131d4e2e328841db0e9223ae4843
jbender2 <300746563@cencol.ca>
2016-09-28 14:01:26 -0400

    Added separate arrays for each restaurant and passing of values from FoodTypes to Restaurants activity.

--- app/src/main/java/com/comp304/group1/assignment2/FoodTypes.java
+++ app/src/main/java/com/comp304/group1/assignment2/FoodTypes.java
@@ -4,18 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class FoodTypes extends AppCompatActivity {
+
+    RadioGroup rg;
+    String selectedValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_types);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        loadUI();
+    }
+
     public void onClickBtnNext2(View v){
+        // Get selected radio button value
+        selectedValue = ((RadioButton)findViewById(rg.getCheckedRadioButtonId())).getText().toString();
 
+        // Create new intent
         Intent i = new Intent(FoodTypes.this, Restaurants.class);
-        startActivity(i);
 
+        // Add radio button value and pass it to the next activity
+        i.putExtra("foodType", selectedValue);
+
+        startActivity(i);
+    }
+
+    public void loadUI(){
+        rg = (RadioGroup) findViewById(R.id.foodTypeRadio);
     }
 }
--- app/src/main/java/com/comp304/group1/assignment2/Restaurants.java
+++ app/src/main/java/com/comp304/group1/assignment2/Restaurants.java
@@ -5,13 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Restaurants extends AppCompatActivity {
 
+    String foodType;
+    ArrayAdapter restaurantAdapter;
+    Spinner restaurantSpinner;
+    List<String> restaurantArray = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.restaurant);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant);
+
+        // Get food preference from previous activity
+        foodType = getIntent().getStringExtra("foodType");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        loadUI();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,11 +41,34 @@ public class Restaurants extends AppCompatActivity {
     }
 
     public void onClickBtnNext3(View v){
-
         Intent i = new Intent(Restaurants.this, AddOrder.class);
         startActivity(i);
-
     }
 
+    public void loadUI(){
+        restaurantSpinner = (Spinner)findViewById(R.id.restaurantSpinner);
+
+        // Set spinner based on restaurant selected in previous activity
+        switch (foodType){
+            case "American":
+                restaurantArray = Arrays.asList(getResources().getStringArray(R.array.a_restaurant_array));
+                break;
+            case "Italian":
+                restaurantArray = Arrays.asList(getResources().getStringArray(R.array.i_restaurant_array));
+                break;
+            case "Chinese":
+                restaurantArray = Arrays.asList(getResources().getStringArray(R.array.c_restaurant_array));
+                break;
+            default:
+                break;
+        }
+
+        // Create Array adapter for restaurant spinner
+        restaurantAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, restaurantArray);
+
+        // Attach the array adapter to the spinner
+        restaurantSpinner.setAdapter(restaurantAdapter);
+    }
 
 }
